d3b2b6a0bea811fda7b02308fe7352c763d5b281
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-07 19:22:45 -0300

    test(app/controllers/default.py): registering rows in database MySQL in my localhost

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -2,8 +2,9 @@ from app import app
 from flask import Flask
 from flask import redirect
 from flask import render_template
-
+from app.models import db
 @app.route('/')
 def index():
-    titlePage = 'TimesJournal'
-    return render_template('index.html', titlePage=titlePage)
+    titlePage = db.cursor.execute("SELECT * FROM posts")
+    results = db.cursor.fetchall()
+    return render_template('index.html', titlePage=results)
