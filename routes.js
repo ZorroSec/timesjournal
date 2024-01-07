0e2bf36e3eebb6e21b19f5fa1e5770d0fa11d840
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-07 11:38:05 -0300

    feat(views/index.html): create a initial route /

--- routes.js
+++ routes.js
@@ -3,3 +3,8 @@ const app = require('./app/app.js')
 const marker = require('marker')
 const path = require('path')
 app.use(express.json())
+
+app.get('/', (req, res)=>{
+    const titlePage = 'TimesJournal'
+    res.sendFile(__dirname + '/views/index.html', titlePage)
+})
