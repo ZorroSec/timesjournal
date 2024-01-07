4b0cbc673bfa58788369aac481c3a34fcc81c064
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-07 20:13:08 -0300

    fix(app/models/db.py): fix bugs in line 8

--- app/models/db.py
+++ app/models/db.py
@@ -1,10 +1,11 @@
 import mysql.connector
 
 conn = mysql.connector.connect(
-    host='localhost',
-    port=3306,
-    user='root',
-    database='timesjournal'
+    host='viaduct.proxy.rlwy.net',
+    port=58384,
+    user='railway',
+    password='orH6IBKTa6CbtYioAuOs4AwNm~-i1.Z0',
+    database='railway'
 )
 
 cursor = conn.cursor()
