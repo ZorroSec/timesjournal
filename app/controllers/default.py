75d7a70cc108b163508a0002812539506699e994
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-08 09:12:31 -0300

    feat(/cadastrar): create new route in default.py

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -8,3 +8,7 @@ def index():
     titlePage = db.cursor.execute("SELECT * FROM posts;")
     results = db.cursor.fetchall()
     return render_template('index.html', results=results)
+
+@app.route('/cadastrar', methods=['GET', 'POST'])
+def cadastrar():
+    return render_template('cadastrar.html')
