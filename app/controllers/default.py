7f089052c4353995c60d62874febf66ddeb41b58
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-08 09:39:08 -0300

    test(/cadastar): test connection in form

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -2,6 +2,7 @@ from app import app
 from flask import Flask
 from flask import redirect
 from flask import render_template
+from flask import request
 from app.models import db
 @app.route('/')
 def index():
@@ -11,4 +12,7 @@ def index():
 
 @app.route('/cadastrar', methods=['GET', 'POST'])
 def cadastrar():
+    if request.method == "POST":
+        nome = request.form['nome']
+        print(nome)
     return render_template('cadastrar.html')
