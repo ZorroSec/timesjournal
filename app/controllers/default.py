a00cd49b321884ced41b8b56e9e8490195fe36e0
José Cipriano <joseiraildesciprianoribeiro@gmail.com>
2024-01-08 11:40:24 -0300

    feat(/cadastrar): register validation

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -19,10 +19,19 @@ def cadastrar():
         email = request.form['email']
         senha = request.form['senha']
         data__atual = date.today()
-        db.cursor.execute(f"INSERT INTO users(nome,email,senha,data,status) VALUES('{nome}','{email}','{senha}','{data__atual}','')")
-        db.conn.commit()
-        results = db.cursor.rowcount
-        print(results)
-        # return "<script>alert('Cadastro realizado com sucesso!!')</script>"
-        redirect('/')
+        # db.cursor.execute(f"INSERT INTO users(nome,email,senha,data,status) VALUES('{nome}','{email}','{senha}','{data__atual}','')")
+        # db.conn.commit()
+        # results = db.cursor.rowcount
+        # print(results)
+        # # return "<script>alert('Cadastro realizado com sucesso!!')</script>"
+        # redirect('/')
+        checkExistUser = db.cursor.execute(f"SELECT * FROM users WHERE nome = '{nome}' and email = '{email}'")
+        results = db.cursor.fetchall()
+        if(results):
+            feedback = """
+<strong>
+    Já existe um perfil com estas informações!!
+</strong>
+"""
+            return render_template('cadastrar.html', feedback=feedback)
     return render_template('cadastrar.html')
